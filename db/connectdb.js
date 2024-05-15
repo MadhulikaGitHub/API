1e8c94d7b7cf750265ae55e79e9b7fb600a45dd4
MadhulikaGitHub <er.madhulikasharma@gmail.com>
2024-05-15 23:14:16 +0530

    commit

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 //const liveDB = 'mongodb+srv://ermadhulikasharma:1234@cluster0.tivz2go.mongodb.net/admission-portal?retryWrites=true&w=majority'
-
+// const liveDB = 'mongodb+srv://ermadhulikasharma:1234@cluster0.irti3lh.mongodb.net/APIEcommerce?retryWrites=true&w=majority&appName=Cluster0'
 const connectDb = ()=>{
     //return mongoose.connect(process.env.LOCAL_URL)
     return mongoose.connect(process.env.LIVE_URL)
--- routes/web.js
+++ routes/web.js
@@ -1,9 +1,34 @@
 const express = require('express')
 const UserController = require('../controllers/UserController')
 const router = express.Router()
+const {ChangeUserAuth} = require('../middleware/auth')
+const ProductController = require('../controllers/ProductController')
+const CategoryController = require('../controllers/CategoryController')
 
 //UserController
-router.get('/getAllUsers', UserController.getAllUsers) 
+router.get('/getAllUsers', UserController.getAllUsers)
+router.get('/admin/getUser/:id', UserController.getSingleUser) 
 router.post('/userInsert', UserController.userInsert) 
+router.post('/verifyLogin', UserController.loginUser)
+router.get('/logout', UserController.logout)
+router.post('/updatePassword', ChangeUserAuth, UserController.updatePassword)
+router.post('/updateProfile', ChangeUserAuth, UserController.updateProfile)
+router.get('/me', ChangeUserAuth, UserController.getUserDetail)
+router.get('/admin/deleteUser/:id', UserController.deleteUser)
+
+// CategoryController
+router.post('/categoryInsert', CategoryController.insertCategory)
+router.get('/getCategory', CategoryController.categoryDisplay)
+router.get('/viewCategory/:id', CategoryController.categoryView)
+router.post('/updateCategory/:id', CategoryController.updateCategory)
+router.get('/deleteCategory/:id', CategoryController.deleteCategory)
+
+// ProductController
+router.get('/products', ProductController.getAllProducts)
+router.get('/getProductDetail/:id', ProductController.getProductDetail)
+router.get('/product/getAdminProduct', ProductController.getAdminProduct)
+router.get('/product/deleteProduct/:id', ProductController.deleteProduct)
+router.post('/product/create', ProductController.createProduct)
+router.post('/product/updateProduct/:id', ProductController.updateProduct)
 
 module.exports = router
